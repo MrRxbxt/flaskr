50a56c7ee5c5bd139e4fb40277c39e7360501062
MrRxbxt <orville.online@gmail.com>
2017-09-11 17:08:31 +0800

    testing-success

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -8,16 +8,16 @@ app.config.from_object(__name__) # load config from this file, flaskr.py
 
 # Load default config and override config from an environment variable
 app.config.update(
-	DATABASE=os.path.join(app.root.path, 'flask.db')
-	SECRET_KEY=b'_5#ysfdfl/',
-	USERNAME='admin'
+	DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+	SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/',
+	USERNAME='admin',
 	PASSWORD='default'
 )
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def connect_db():
 	"""connects to the specific database"""
-	rv  = sqlite3.connect(app)
+	rv  = sqlite3.connect(app.config['DATABASE'])
 	rv.row_factory = sqlite3.Row
 	return rv
 
@@ -60,7 +60,7 @@ def show_entries():
 def add_entry():
 	if not session.get('logged_in'):
 		abort(401)
-	db = get_db(0
+	db = get_db()
 	db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
 	db.commit()
 	flash('New entry was successfully posted')
@@ -70,13 +70,19 @@ def add_entry():
 def login():
 	error = None
 	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME']
+		if request.form['username'] != app.config['USERNAME']:
 			error = 'Invalid username'
-		elif request.form['password'] != app.config['PASSWORD']
+		elif request.form['password'] != app.config['PASSWORD']:
 			error = 'Invalid password'
 		else:
 			session['logged_in'] = True
 			flash('You were logged in')
 			return redirect(url_for('show_entries'))
 	return render_template('login.html', error=error)
+	
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
 	
